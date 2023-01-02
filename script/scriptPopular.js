64ccd77fafba36ecbd98c1505e2acb5339520573
Cykkyb <shiniaiev00@mail.ru>
2023-01-02 14:56:22 +0300

    popular-slider, offers

---
+++ script/scriptPopular.js
@@ -0,0 +1,39 @@
+new Swiper('.popular-slider', {
+
+    loop: true,
+
+    navigation: {
+        nextEl: '.popular-slider__button-next',
+        prevEl: '.popular-slider__button-prev',
+    },
+    spaceBetween: 25,
+    breakpoints: {
+        320: {
+            slidesPerView: 1.2,
+        },
+        567: {
+            slidesPerView: 1.5,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1200: {
+            slidesPerView: 3,
+        }
+    },
+})
+
+
+
+
+let q = $('.popular-slider__quantity')
+
+for (let i = 0; i < q.length; i++){
+    if(q[i].innerText == 0){
+        $('.popular-slider__status')[i].innerText = 'Отсутствует'
+    }else {
+        $('.popular-slider__status')[i].innerText = 'В наличие'
+    }
+}
+
+
---
+++ script/scriptSlider.js
@@ -0,0 +1,10 @@
+new Swiper('.slider', {
+
+    loop: true,
+
+    pagination:{
+        el:'.swiper-pagination',
+        clickable: true,
+    },
+
+})
