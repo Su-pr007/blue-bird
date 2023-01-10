e52fd6182e4d1384324b65174eed276be1ed5ecc
Cykkyb <shiniaiev00@mail.ru>
2023-01-10 22:34:24 +0300

    что-то сделал

---
+++ www/blocks/offers.php
@@ -0,0 +1,56 @@
+  <div class="offers">
+            <div class="container1">
+
+                <div class="offers__label">Предложения</div>
+
+                <div class="offers-grid">
+                    <div class="offers-grid__item">
+                        <div class="offers-grid__item-wrapper">
+                            <img src="icon/offers/1.jpg" alt="" class="offers-grid__image">
+                            <div class="offers-grid__label">Голубь</div>
+                        </div>
+                    </div>
+                    <div class="offers-grid__item">
+                        <div class="offers-grid__item-wrapper">
+                            <img src="icon/offers/2.jpg" alt="" class="offers-grid__image">
+                            <div class="offers-grid__label">Сова</div>
+                        </div>
+                    </div>
+                    <div class="offers-grid__item">
+                        <div class="offers-grid__item-wrapper">
+                            <img src="icon/offers/3.jpg" alt="" class="offers-grid__image">
+                            <div class="offers-grid__label">Павлин</div>
+                        </div>
+                    </div>
+                    <div class="offers-grid__item">
+                        <div class="offers-grid__item-wrapper">
+                            <img src="icon/offers/4.jpg" alt="" class="offers-grid__image">
+                            <div class="offers-grid__label">Пин</div>
+                        </div>
+                    </div>
+                    <div class="offers-grid__item">
+                        <div class="offers-grid__item-wrapper">
+                            <img src="icon/offers/5.jpg" alt="" class="offers-grid__image">
+                            <div class="offers-grid__label"></div>
+                        </div>
+                    </div>
+                    <div class="offers-grid__item">
+                        <div class="offers-grid__item-wrapper">
+                            <img src="icon/offers/6.jpg" alt="" class="offers-grid__image">
+                            <div class="offers-grid__label">Воробушек</div>
+                        </div>
+                    </div>
+
+                    <div class="offers-grid__item">
+                        <div class="offers-grid__item-wrapper">
+                            <img src="icon/offers/7.jpg" alt="" class="offers-grid__image">
+                            <div class="offers-grid__label">Тукан</div>
+                        </div>
+                    </div>
+
+                    <button class="offers-grid__all">
+                        Паказать всё
+                    </button>
+                </div>
+            </div>
+        </div>
